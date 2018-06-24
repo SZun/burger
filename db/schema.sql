44d091452273e1c1cd2235452dab7ec66bb9c061
Sam G. Zun <zunsamuel97@gmail.com>
2018-06-24 02:10:23 -0500

    Setting up my MySQL schema and seeds

---
+++ db/schema.sql
@@ -0,0 +1,13 @@
+DROP DATABASE IF EXISTS burgers_db;
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+CREATE TABLE burgers (
+    id INTEGER(40) AUTO_INCREMENT NOT NULL,
+    burger_name VARCHAR(255) NOT NULL,
+    devoured BOOLEAN NOT NULL DEFAULT 0,
+    PRIMARY KEY (id)
+);
+
+SELECT * FROM burgers;
---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (id,burger_name) VALUES(1,"Bash Burger");
+INSERT INTO burgers (id,burger_name) VALUES(2,"Bae Burger");
+INSERT INTO burgers (id,burger_name) VALUES(3,"Upland Burger");
